0df1966aa765fcae13f9d278d21749c575aa4129
Aminourislam <mdamenourislam@gmail.com>
2026-05-22 15:23:36 +0600

    Medical validator & basic about module

---
+++ module_importing.py
@@ -0,0 +1,17 @@
+# from math import sqrt as root, sin, radians,pi
+# degree = 30
+# rad = radians(degree)
+# print(sin(rad))
+# print(root(50))
+# print(pi)
+
+#if __name__ == '__main__':
+
+
+
+import datetime as dt
+
+Birthday = dt.date(2007,9,30)
+print(Birthday.day)
+print(Birthday.year)
+print(Birthday.month)
---
+++ testing.py
@@ -0,0 +1,15 @@
+# from math import sqrt as root, sin, radians,pi
+# degree = 30
+# rad = radians(degree)
+# print(sin(rad))
+# print(root(50))
+# print(pi)
+
+if __name__ == '__main__':
+	print(3)
+import datetime as dt
+
+Birthday = dt.date(2007,9,30)
+print(Birthday.day)
+print(Birthday.year)
+print(Birthday.month)
