0f3b9f7dcd3d516d3c526f87ed5fa28402cea326
Md Aminour Islam Sayad <148259659+Aminourislam@users.noreply.github.com>
2026-06-22 23:19:48 +0600

    👍 this Build_a_planet.py

--- class/Build_a_planet.py
+++ class/Build_a_planet.py
@@ -23,7 +23,36 @@ You should create three instances of the Planet class named planet_1, planet_2, 
 You should print each planet object to see the __str__ method output.
 You should call the orbit method on each planet object and print the result.
 '''
-Planet: {name} | Type: {planet_type} | Star: {star}
+class Planet:
+    def __init__(self, name, planet_type, star):
+        if not all(isinstance(item, str) for item in [name, planet_type, star]):
+            raise TypeError("name, planet type, and star must be strings")
+
+        if any(item == "" for item in [name, planet_type, star]):
+            raise ValueError("name, planet_type, and star must be non-empty strings")
+
+        self.name = name
+        self.planet_type = planet_type
+        self.star = star
+
+    def orbit(self):
+        return f"{self.name} is orbiting around {self.star}..."
+
+    def __str__(self):
+        return f"Planet: {self.name} | Type: {self.planet_type} | Star: {self.star}."
+
+
+planet_1 = Planet("Earth", "Terrestrial", "Sun")
+planet_2 = Planet("Jupiter", "Gas Giant", "Sun")
+planet_3 = Planet("Mars", "Terrestrial", "Sun")
+
+print(planet_1)
+print(planet_2)
+print(planet_3)
+
+print(planet_1.orbit())
+print(planet_2.orbit())
+print(planet_3.orbit())
 
 '''
 Tests:
